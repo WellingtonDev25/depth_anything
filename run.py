5d25c4f9db1f6d8d1bf328e7b8364d3d378e1845
WellingtonDev25 <56238012+WellingtonDev25@users.noreply.github.com>
2024-02-21 13:35:43 -0300

    Add files via upload

---
+++ run.py
@@ -0,0 +1,108 @@
+import cv2
+import numpy as np
+import torch
+import torch.nn.functional as F
+from torchvision.transforms import Compose
+
+
+from depth_anything.dpt import DepthAnything
+from depth_anything.util.transform import Resize, NormalizeImage, PrepareForNet
+
+
+#encoders = ['vits', 'vitb', 'vitl']
+encoder = 'vits'
+
+margin_width = 50
+caption_height = 60
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+font_scale = 1
+font_thickness = 2
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+depth_anything = DepthAnything.from_pretrained('LiheYoung/depth_anything_{}14'.format(encoder)).to(DEVICE)
+
+total_params = sum(param.numel() for param in depth_anything.parameters())
+print('Total parameters: {:.2f}M'.format(total_params / 1e6))
+
+depth_anything.eval()
+
+transform = Compose([
+    Resize(
+        width=518,
+        height=518,
+        resize_target=False,
+        keep_aspect_ratio=True,
+        ensure_multiple_of=14,
+        resize_method='lower_bound',
+        image_interpolation_method=cv2.INTER_CUBIC,
+    ),
+    NormalizeImage(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    PrepareForNet(),
+])
+
+
+# cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+cap = cv2.VideoCapture('heat.mp4')
+
+while cap.isOpened():
+    ret, raw_image = cap.read()
+
+    if not ret:
+        break
+
+    raw_image = cv2.resize(raw_image, (640, 480))
+
+    image = cv2.cvtColor(raw_image, cv2.COLOR_BGR2RGB) / 255.0
+    
+    h, w = image.shape[:2]
+    
+    image = transform({'image': image})['image']
+    image = torch.from_numpy(image).unsqueeze(0).to(DEVICE)
+    
+    with torch.no_grad():
+        depth = depth_anything(image)
+    
+    depth = F.interpolate(depth[None], (h, w), mode='bilinear', align_corners=False)[0, 0]
+    depth = (depth - depth.min()) / (depth.max() - depth.min()) * 255.0
+    
+    depth = depth.cpu().numpy().astype(np.uint8)
+    depth_color = cv2.applyColorMap(depth, cv2.COLORMAP_INFERNO)
+    
+    split_region = np.ones((raw_image.shape[0], margin_width, 3), dtype=np.uint8) * 255
+    combined_results = cv2.hconcat([raw_image, split_region, depth_color])
+    
+    caption_space = np.ones((caption_height, combined_results.shape[1], 3), dtype=np.uint8) * 255
+    captions = ['Raw image', 'Depth Anything']
+    segment_width = w + margin_width
+    for i, caption in enumerate(captions):
+        # Calculate text size
+        text_size = cv2.getTextSize(caption, font, font_scale, font_thickness)[0]
+
+        # Calculate x-coordinate to center the text
+        text_x = int((segment_width * i) + (w - text_size[0]) / 2)
+
+        # Add text caption
+        cv2.putText(caption_space, caption, (text_x, 40), font, font_scale, (0, 0, 0), font_thickness)
+    
+    final_result = cv2.vconcat([caption_space, combined_results])
+
+    # Write the frame to the video file
+    cv2.imshow('Depth Anything',cv2.resize(final_result,(1280,720)))
+
+    # Press q on keyboard to exit
+    if cv2.waitKey(1) ==27:
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
